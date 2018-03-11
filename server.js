fef13f01bedb70fc6a59870a5ca743d165fc9608
Katy <ihatetoast@gmail.com>
2018-03-11 14:25:48 -0500

    filled out server. connected to db.

--- server.js
+++ server.js
@@ -12,19 +12,61 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const exphbs = require('express-handlebars');
+const hbs = require('express-handlebars');
 
 const mongoose = require('mongoose');
 //cheerio and axios required in the scraper.js
 
-//set PORT that's Heroku ready, Freddy
-const PORT = process.env.PORT || 3000;
 
 //fire up express
 const app = express();
+
+//set up my babydase:
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/scraperdb';
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {});
+
+// mongoose.connect(MONGODB_URI.database, err =>{
+//   if(err){
+//     console.log("Like a bad date, there's no connection.");
+//   } else {
+//     console.log("Bingo bango bongo you're connected to the Mongo oh oh ohoh oh ohhh!");
+//   }
+// });
+var db = mongoose.connection;
+
+//handlebars templating engine
+app.engine('hbs',  hbs({extname: 'hbs',defaultLayout:'main',layoutsDir: __dirname + '/views/layouts/'}));
+//path to layout:
+app.set('views', path.join(__dirname, 'views'));
+//make it so
+app.set('view engine', 'hbs');
+
+
+//MIDDLEWARE
+//log requests
+app.use(logger("dev"));
+
+//set up static access to public dir:
+app.use(express.static(path.join(__dirname, '/public')));
+
+//body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//ROUTES! 
+const routes = require('./routes')
+
+//set PORT that's Heroku ready, Freddy
+const PORT = process.env.PORT || 3000;
+
+
+
+
+
 
 
 //start the serva, Minerva
 app.listen(PORT, function(){
   console.log(`All the cool kids are scrapin' on ${PORT}`);
-})
+});
