04306768a81c4707fde56bfb312a68844bc9f149
Katy <ihatetoast@gmail.com>
2018-03-19 12:03:48 -0500

    adding notes for controllers and those hurt my Brians

---
+++ controllers/fetch.js
@@ -0,0 +1,7 @@
+//require the models.
+//also require the scraper fcn as ... hello. scrape and fetch
+
+//this will scrap eht headline first
+//then create the db table
+
+//then any logic pertaining to new articles or not
---
+++ controllers/note.js
@@ -0,0 +1,7 @@
+//require the model
+
+//do the find one and return the note
+
+//create a note to read. save to db. 
+
+//delete by removing params:id
